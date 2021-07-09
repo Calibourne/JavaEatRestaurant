2d72c1cfb5c29954930329ed32c944245a8dc665
Calibourne <Eddiekanevsky@gmail.com>
2021-07-09 18:00:20 +0300

    Added new class for later use (the bonus section of pictures)

---
+++ src/View/newElements/imageListCell.java
@@ -0,0 +1,29 @@
+package View.newElements;
+
+import Model.Customer;
+import Model.Person;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * A class for displaying customer's profile picture
+ */
+public class imageListCell extends ListCell<Person> {
+    @Override
+    protected void updateItem(Person item, boolean isEmpty) {
+        super.updateItem(item, isEmpty);
+        setGraphic(null);
+        setText(null);
+        if(item!=null){
+            if(item instanceof Customer) {
+                Customer c = (Customer) item;
+                Image img = new Image("");
+                ImageView imageView = new ImageView(img);
+                setGraphic(imageView);
+                setText(c.toString());
+            }
+        }
+    }
+}
+

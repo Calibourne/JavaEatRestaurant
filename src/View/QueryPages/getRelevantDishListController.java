eb05aae89dd861f73f0a70982d66dcdedad88a2d
Calibourne <Eddiekanevsky@gmail.com>
2021-08-06 09:47:23 +0300

    Fixed the getRelevantDishListController, now it's 100% complete

--- src/View/QueryPages/getRelevantDishListController.java
+++ src/View/QueryPages/getRelevantDishListController.java
@@ -1,13 +1,19 @@
 package View.QueryPages;
 
 import Model.Customer;
+import Model.Dish;
 import Model.Restaurant;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class getRelevantDishListController {
 
@@ -28,20 +34,31 @@ public class getRelevantDishListController {
         try{
             Restaurant rest = Restaurant.getInstance();
             customer_combobox.getItems().addAll(rest.getCustomers().values());
+            customer_combobox.valueProperty().addListener((opt, oldValue, newValue)->{
+                if(!newValue.equals(oldValue)){
+                    query_result.getItems().clear();
+                    if(rest.getCustomers().containsValue(newValue)) {
+                        List<String> res = rest.getReleventDishList(newValue).stream().map(Dish::toString).toList();
+                        res.forEach(r -> query_result.getItems().add(r));
+                    }
+                }
+            });
 
         }catch (NullPointerException ex){
             System.out.println(ex.getMessage());
         }
     }
 
-    // currently not working, need to fix
+    // No more relevant
     public void buttonPressed(ActionEvent event){
         try {
             Restaurant rest = Restaurant.getInstance();
 
             if(rest.getCustomers().containsValue(customer_combobox.getSelectionModel().getSelectedItem())){
                 //query_result.setText(rest.getRelevantDishList(customer_combobox.getSelectionModel().getSelectedItem());
-                query_result.getItems().addAll(rest.getReleventDishList(customer_combobox.getSelectionModel().getSelectedItem()).toString());
+                //query_result.getItems().addAll(rest.getReleventDishList(customer_combobox.getSelectionModel().getSelectedItem()).toString());
+                List<String> res = rest.getReleventDishList(customer_combobox.getValue()).stream().map(Dish::toString).toList();
+                res.forEach(r->query_result.getItems().add(r));
                 System.out.println("test");
             }
 

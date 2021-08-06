a965d528e66c6ab6913d74b349c3a8afa1123df7
Daniel-Himself <danielsharon97@gmail.com>
2021-08-06 15:54:03 +0300

    GetCooksByExpertise query 100% working

--- src/View/QueryPages/getCooksByExpertiseController.java
+++ src/View/QueryPages/getCooksByExpertiseController.java
@@ -1,12 +1,19 @@
 package View.QueryPages;
 
+import Model.Cook;
+import Model.Dish;
 import Model.Restaurant;
 import Utils.Expertise;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class getCooksByExpertiseController {
 
@@ -17,6 +24,14 @@ public class getCooksByExpertiseController {
     private ComboBox<Expertise> expertise_combobox;
 
     @FXML
+    private ListView<String> query_result;
+
+    @FXML
+    private Button query_button;
+
+
+
+    @FXML
     private void initialize(){
         try{
             expertise_combobox.getItems().addAll(Arrays.stream(Expertise.values()).toList());
@@ -24,5 +39,39 @@ public class getCooksByExpertiseController {
         }catch (NullPointerException ex){
             System.out.println(ex.getMessage());
         }
+    }
+
+    // not working atm for some reason
+    public void buttonPressed(ActionEvent event){
+        try {
+
+            //Daniel's attempt at clearing the list for each button press, doesn't work atm
+            if(!(query_result.getItems().isEmpty())){
+
+                query_result.getItems().clear();
+            }
+
+            Restaurant rest = Restaurant.getInstance();
+
+            if(!(rest.getCooksByExpertise(expertise_combobox.getSelectionModel().getSelectedItem()).values().isEmpty())){
+
+                List<String> results = rest.getCooksByExpertise(expertise_combobox.getValue()).values().stream().map(Cook::toString).toList();
+                results.forEach(r->query_result.getItems().add(r));
+
+            }
+            else{
+                if(!(query_result.getItems().isEmpty())){
+
+                    query_result.getItems().clear();
+                }
+                query_result.getItems().add("No cooks available for the selected expertise");
+
+            }
+
+
+        }catch (NullPointerException ex){
+            System.out.println(ex.getMessage());
+        }
+
     }
 }

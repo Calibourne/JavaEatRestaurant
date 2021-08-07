f6a47847d43e1c0ba74e701a2c917d4e67e6a73b
Daniel-Himself <danielsharon97@gmail.com>
2021-08-07 19:22:31 +0300

    Profit relation quest done

--- src/View/QueryPages/getProfitRelationController.java
+++ src/View/QueryPages/getProfitRelationController.java
@@ -1,4 +1,63 @@
 package View.QueryPages;
 
+import Model.Dish;
+import Model.Restaurant;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
 public class getProfitRelationController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane pnlQueries;
+
+    @FXML
+    private ListView<String> query_result;
+
+    Restaurant rest = Restaurant.getInstance();
+    @FXML
+    void initialize() {
+        assert pnlQueries != null : "fx:id=\"pnlQueries\" was not injected: check your FXML file 'getProfitRelation.fxml'.";
+        assert query_result != null : "fx:id=\"query_result\" was not injected: check your FXML file 'getProfitRelation.fxml'.";
+
+        try {
+
+            //Reset the result list each time a query is called upon
+            if(!(query_result.getItems().isEmpty())){
+
+                query_result.getItems().clear();
+            }
+
+            if(!(rest.getProfitRelation().isEmpty())){
+
+                List<String> results = rest.getProfitRelation().stream().map(Dish::toString).toList();
+                results.forEach(r->query_result.getItems().add(r));
+
+            }
+            else{
+                if(!(query_result.getItems().isEmpty())){
+
+                    query_result.getItems().clear();
+                }
+                query_result.getItems().add("There are no ingredient to show at the moment.");
+
+            }
+
+
+        }catch (NullPointerException ex){
+            System.out.println(ex.getMessage());
+        }
+
+
+    }
 }

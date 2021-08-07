371d2ee89f406ed196f7ac65215f76f8b65c3d0d
Daniel-Himself <danielsharon97@gmail.com>
2021-08-07 12:21:58 +0300

    RevenueFromExpress is done

--- src/View/QueryPages/revenueFromExpressDeliveriesController.java
+++ src/View/QueryPages/revenueFromExpressDeliveriesController.java
@@ -1,4 +1,37 @@
 package View.QueryPages;
 
+import Model.Restaurant;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class revenueFromExpressDeliveriesController {
+
+    @FXML
+    private AnchorPane pnlQueries;
+
+    @FXML
+    private Label query_result;
+
+    public void initialize(){
+        try {
+
+            Restaurant restaurant = Restaurant.getInstance();
+
+            //Reset the result list each time a query is called upon
+            if(!(query_result.getText().isEmpty())){
+
+                query_result.setText("");
+            }
+
+            query_result.setText(String.format("%.2f ₪",restaurant.revenueFromExpressDeliveries()));
+
+
+
+        }catch (NullPointerException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
 }

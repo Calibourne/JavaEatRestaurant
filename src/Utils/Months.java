c95daf4feb2fa93ffe2f86f62c6c1165ea0b1860
Calibourne <Eddiekanevsky@gmail.com>
2021-08-07 18:15:42 +0300

    Utility enum added

---
+++ src/Utils/Months.java
@@ -0,0 +1,24 @@
+package Utils;
+
+public enum Months {
+    January(1),
+    February(2),
+    March(3),
+    April(4),
+    May(5),
+    June(6),
+    July(7),
+    August(8),
+    September(9),
+    October(10),
+    November(11),
+    December(12);
+    private final int value;
+    Months(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}

54f5367e65797b636da3b8fde5a263996aa78fa9
Daniel-Himself <danielsharon97@gmail.com>
2021-08-12 20:33:47 +0300

    4 out of 8 customer menu buttons now fully functioning

--- src/View/CustomerStage/CustomerHomeController.java
+++ src/View/CustomerStage/CustomerHomeController.java
@@ -25,7 +25,7 @@ public class CustomerHomeController {
     private ResourceBundle resources;
 
     @FXML
-    private StackPane query_pane;
+    private StackPane content_pane;
 
     @FXML
     private URL location;
@@ -43,7 +43,7 @@ public class CustomerHomeController {
     private Button relevant_dish_menu_button;
 
     @FXML
-    private Button btnEditRecords;
+    private Button cooks_expertise_button;
 
     @FXML
     private Button popular_ingredients_button;
@@ -68,7 +68,7 @@ public class CustomerHomeController {
             assert homepage_button != null : "fx:id=\"homepage_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
             assert new_order_button != null : "fx:id=\"new_order_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
             assert relevant_dish_menu_button != null : "fx:id=\"relevant_dish_menu_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-            assert btnEditRecords != null : "fx:id=\"btnEditRecords\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert cooks_expertise_button != null : "fx:id=\"cooks_expertise_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
             assert popular_ingredients_button != null : "fx:id=\"popular_ingredients_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
             assert view_menu_button != null : "fx:id=\"view_menu_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
             assert settings_button != null : "fx:id=\"settings_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
@@ -85,43 +85,43 @@ public class CustomerHomeController {
         @FXML
         public void handleButtonClick(ActionEvent event) {
             try{
-                Node queryPage;
+                Node loadedPage;
                 switch(((Node)event.getSource()).getId()){
 //                    case "profit_relation_button":
-//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getProfitRelation.fxml")));
-//                        query_pane.getChildren().add(queryPage);
+//                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getProfitRelation.fxml")));
+//                        content_pane.getChildren().add(loadedPage);
 //                        break;
 //                    case "ai_macine_button":
-//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/createAIMacine.fxml")));
-//                        query_pane.getChildren().add(queryPage);
+//                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/createAIMacine.fxml")));
+//                        content_pane.getChildren().add(loadedPage);
 //                        break;
 //                    case "express_revenue_button":
-//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/revenueFromExpressDeliveries.fxml")));
-//                        query_pane.getChildren().add(queryPage);
+//                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/revenueFromExpressDeliveries.fxml")));
+//                        content_pane.getChildren().add(loadedPage);
 //                        break;
 //                    case "delis_by_type_button":
-//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getNumberOfDeliveriesPerType.fxml")));
-//                        query_pane.getChildren().add(queryPage);
+//                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getNumberOfDeliveriesPerType.fxml")));
+//                        content_pane.getChildren().add(loadedPage);
 //                        break;
 //                    case "delis_by_dp_button":
-//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getDeliveriesByPerson.fxml")));
-//                        query_pane.getChildren().add(queryPage);
+//                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getDeliveriesByPerson.fxml")));
+//                        content_pane.getChildren().add(loadedPage);
 //                        break;
-//                    case "order_waiting_time_button":
-//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/orderWaitingTime.fxml")));
-//                        query_pane.getChildren().add(queryPage);
-//                        break;
-                    case "popular_ingredients_button":
-                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../CustomerStage/getPopularComponent.fxml")));
-                        query_pane.getChildren().add(queryPage);
+                    case "view_menu_button":
+                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../ViewStructuresPages/ViewDishes.fxml")));
+                        content_pane.getChildren().add(loadedPage);
                         break;
-//                    case "cooks_by_expertise_button":
-//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getCooksByExpertise.fxml")));
-//                        query_pane.getChildren().add(queryPage);
-//                        break;
+                    case "popular_ingredients_button":
+                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getPopularComponent.fxml")));
+                        content_pane.getChildren().add(loadedPage);
+                        break;
+                    case "cooks_expertise_button":
+                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getCooksByExpertise.fxml")));
+                        content_pane.getChildren().add(loadedPage);
+                        break;
 //                    case "relevant_dish_list_button":
-//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getRelevantDishList.fxml")));
-//                        query_pane.getChildren().add(queryPage);
+//                        loadedPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getRelevantDishList.fxml")));
+//                        content_pane.getChildren().add(loadedPage);
 //                        break;
 
                 }

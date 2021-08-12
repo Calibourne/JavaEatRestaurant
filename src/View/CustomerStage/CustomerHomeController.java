eb3fb4891f0ba5f018b8e6996615d00d40d9b0c6
Daniel-Himself <danielsharon97@gmail.com>
2021-08-12 20:05:51 +0300

    Customer- reduced window size, implemented popular comp query

--- src/View/CustomerStage/CustomerHomeController.java
+++ src/View/CustomerStage/CustomerHomeController.java
@@ -6,20 +6,26 @@ import View.Controllers.LoginPageController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class CustomerHomeController {
 
     @FXML
     private ResourceBundle resources;
+
+    @FXML
+    private StackPane query_pane;
 
     @FXML
     private URL location;
@@ -51,56 +57,80 @@ public class CustomerHomeController {
     @FXML
     private Button signout_button;
 
-    @FXML
-    void handleButtonClick(ActionEvent event) {
-
-    }
 
     Restaurant restaurant = Restaurant.getInstance();
     private Customer customer;
 
     @FXML
     void initialize() {
-        try{
-        assert username_label != null : "fx:id=\"username_label\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-        assert homepage_button != null : "fx:id=\"homepage_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-        assert new_order_button != null : "fx:id=\"new_order_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-        assert relevant_dish_menu_button != null : "fx:id=\"relevant_dish_menu_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-        assert btnEditRecords != null : "fx:id=\"btnEditRecords\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-        assert popular_ingredients_button != null : "fx:id=\"popular_ingredients_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-        assert view_menu_button != null : "fx:id=\"view_menu_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-        assert settings_button != null : "fx:id=\"settings_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
-        assert signout_button != null : "fx:id=\"signout_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+        try {
+            assert username_label != null : "fx:id=\"username_label\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert homepage_button != null : "fx:id=\"homepage_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert new_order_button != null : "fx:id=\"new_order_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert relevant_dish_menu_button != null : "fx:id=\"relevant_dish_menu_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert btnEditRecords != null : "fx:id=\"btnEditRecords\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert popular_ingredients_button != null : "fx:id=\"popular_ingredients_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert view_menu_button != null : "fx:id=\"view_menu_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert settings_button != null : "fx:id=\"settings_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
+            assert signout_button != null : "fx:id=\"signout_button\" was not injected: check your FXML file 'CustomerHome.fxml'.";
 
-        customer = LoginPageController.getCustomer();
+            customer = LoginPageController.getCustomer();
 //        username_label.setText("Welcome, \n"+customer.getFirstName()+" "+customer.getLastName()); This makes the label span on two lines
-            username_label.setText("Welcome, "+customer.getFirstName()+" "+customer.getLastName());
+            username_label.setText("Welcome, " + customer.getFirstName() + " " + customer.getLastName());
 
-
-
-
-
-        }catch(NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println(e.getMessage());
         }
-
-
-        // Change label below profile pic to logged-in username / first+last names
-//        try {
-//            if(!(username_label.getText().isEmpty())){
-//
-//                username_label.setText("");
-//            }
-//
-//            username_label.setText();
-//
-//
-//
-//        }catch (NullPointerException ex){
-//            System.out.println(ex.getMessage());
-//        }
-
     }
+        @FXML
+        public void handleButtonClick(ActionEvent event) {
+            try{
+                Node queryPage;
+                switch(((Node)event.getSource()).getId()){
+//                    case "profit_relation_button":
+//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getProfitRelation.fxml")));
+//                        query_pane.getChildren().add(queryPage);
+//                        break;
+//                    case "ai_macine_button":
+//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/createAIMacine.fxml")));
+//                        query_pane.getChildren().add(queryPage);
+//                        break;
+//                    case "express_revenue_button":
+//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/revenueFromExpressDeliveries.fxml")));
+//                        query_pane.getChildren().add(queryPage);
+//                        break;
+//                    case "delis_by_type_button":
+//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getNumberOfDeliveriesPerType.fxml")));
+//                        query_pane.getChildren().add(queryPage);
+//                        break;
+//                    case "delis_by_dp_button":
+//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getDeliveriesByPerson.fxml")));
+//                        query_pane.getChildren().add(queryPage);
+//                        break;
+//                    case "order_waiting_time_button":
+//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/orderWaitingTime.fxml")));
+//                        query_pane.getChildren().add(queryPage);
+//                        break;
+                    case "popular_ingredients_button":
+                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../CustomerStage/getPopularComponent.fxml")));
+                        query_pane.getChildren().add(queryPage);
+                        break;
+//                    case "cooks_by_expertise_button":
+//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getCooksByExpertise.fxml")));
+//                        query_pane.getChildren().add(queryPage);
+//                        break;
+//                    case "relevant_dish_list_button":
+//                        queryPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../QueryPages/getRelevantDishList.fxml")));
+//                        query_pane.getChildren().add(queryPage);
+//                        break;
+
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
 
     public void signout(){
         try {
---
+++ src/View/CustomerStage/getPopularComponentController.java
@@ -0,0 +1,52 @@
+package View.CustomerStage;
+
+import Model.Component;
+import Model.Restaurant;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.List;
+
+public class getPopularComponentController {
+
+    @FXML
+    private AnchorPane pnlQueries;
+
+    @FXML
+    private ListView<String> query_result;
+
+    public void initialize(){
+        try {
+
+            //Reset the result list each time a query is called upon
+            if(!(query_result.getItems().isEmpty())){
+
+                query_result.getItems().clear();
+            }
+
+            Restaurant rest = Restaurant.getInstance();
+
+            if(!(rest.getPopularComponents().isEmpty())){
+
+                List<String> results = rest.getPopularComponents().stream().map(Component::toString).toList();
+                results.forEach(r->query_result.getItems().add(r));
+
+            }
+            else{
+                if(!(query_result.getItems().isEmpty())){
+
+                    query_result.getItems().clear();
+                }
+                query_result.getItems().add("There are no ingredient to show at the moment.");
+
+            }
+
+
+        }catch (NullPointerException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+}
